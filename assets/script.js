306262dcfbf1ad0fd5bd3f85b9b589267fd993bb
BenHwaet <benmdesroches@gmail.com>
2023-06-02 22:43:54 -0300

    added time-blocks to html and assigned hourly values from 6am to 12am; started some CSS styling; wrote in draft of logic needed to accomplish goals in Javascript

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,20 @@
+$(document).ready(function() {
+//   var present = dayjs.tz(dateValue);
+//   var currentTime = (present.format('dddd, MMMM D YYYY, h:mm:ss a'))
+// $("#currentDay").text(present);
+var currentDay = $("#currentDay");
+var present = dayjs();
+
+
+  // do stuff when DOM is ready
+
+var currentDay = $("#currentDay");
+
+dayjs().format();
+var timeBlock = $(".time-block");
+
+console.log(present)
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -8,17 +25,37 @@ $(function () {
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
-    //
+
+// addEventListener to saveBtn, to target time-block by using this syntax
+// to identify time-block container and save text data to local storage.
+
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
+    
+//DRAFT LOGIC for color application according to time: - make variables
+// if (dayjs(past).isBefore(present)) {
+//   then addClass to Element, linked to
+//   <bg-color> of "time-block" is grey;
+// } else if (dayjs(present)) {
+//   then addClass to Element, linked to
+//   <bg-color> of "time-block" is red or orange;
+// } else if (dayjs(future).isAfter(present))
+//  then addClass to Element, linked to
+//  <bg-color> of "time-block" is green; --> probably will change colors.
+
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
+
+// associate textarea of hourX to hourX saveBtn
+// write into local storage
+    
     // TODO: Add code to display the current date in the header of the page.
+
+ // write-in text of current dayjs value to html
   });
   
+});
