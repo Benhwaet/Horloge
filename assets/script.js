21e070d8ec367334bb47d51bd218ae476e8113a3
BenHwaet <benmdesroches@gmail.com>
2023-06-05 12:02:21 -0300

    added working function to mark time status as past, present, and/or future in coded colors provided through CSS. Used 'this' syntax to loop through each timeBlock and compare individual form IDs

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,69 @@
-$(document).ready(function() {
-//   var present = dayjs.tz(dateValue);
-//   var currentTime = (present.format('dddd, MMMM D YYYY, h:mm:ss a'))
-// $("#currentDay").text(present);
-var currentDay = $("#currentDay");
-var present = dayjs().format("dddd, MMMM D YYYY, HH:mm a");
+var title = $("h1");
+var lead = $(".lead");
+//display real time in lead
+// TODO: Add code to display the current date in the header of the page.
+var display = dayjs().format("dddd, MMMM D YYYY, HH:mm a");
+var horloge = $("#horloge");
+horloge.text(display);
 
-currentDay.text(present);
+var calendar = $(".calendar");
 
-var presentHour = dayjs().format("HH");
-var roundHour = (presentHour + ":00");
-console.log(roundHour);
-var hourText = $(".hour-text");
 
+
+// to use in comparison with form ids [06, 07, 08, etc.]
+var currentHour = dayjs().hour();
+console.log(currentHour);
+//To set saveBtn event
+var saveBtn = $(".saveBtn");
+var labelText = $(".hour-text").val();
+
+var task = $(".task");
 var timeBlock = $(".time-block");
-console.log(timeBlock.length);
-var label = $("<label>");
-  // do stuff when DOM is ready
-function status(label){
-// for (i=0; 0 <= timeBlock.length; i++) {
-  if (roundHour === hourText) {
-  timeBlock.addClass("past");
-} else if (roundHour > hourText) {
-  timeBlock.addClass("present");
-} else if (roundHour < hourText) {
-  timeBlock.addClass("future");
-}
-};
+console.log(timeBlock);
 
-status();
-// dayjs().format();
-
-});
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
+$(document).ready(function() {
+  
+ // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
 
-// addEventListener to saveBtn, to target time-block by using this syntax
-// to identify time-block container and save text data to local storage.
+// addEventListener to saveBtn, to target time-block by using 'this' syntax
+// to identify approptiate time-block container and save text data to local storage.
 
+saveBtn.on("click", function(event) {
+  localStorage.setItem("taskText", task);
+
+  console.log(task);
+  
+})
+    // TODO: Add code to get any user input that was saved in localStorage and set
+    // the values of the corresponding textarea elements. HINT: How can the id
+    // attribute of each time-block be used to do this?
+
+// associate textarea of hourX to hourX saveBtn
+// write into local storage
+
+function status() {
+timeBlock.each(function(){
+  var formID = $(this).attr("id");
+  console.log(formID);
+  if (formID < currentHour) {
+    $(this).addClass("past");
+  } else if (formID == currentHour){
+    $(this).removeClass("past")
+    $(this).addClass("present");
+  } else if (formID > currentHour){
+    $(this).addClass("future");
+}
+});
+
+} status();
+
+});
+   
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
@@ -63,15 +81,9 @@ $(function () {
 //  then addClass to Element, linked to
 //  <bg-color> of "time-block" is green; --> probably will change colors.
 
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
 
-// associate textarea of hourX to hourX saveBtn
-// write into local storage
     
-    // TODO: Add code to display the current date in the header of the page.
+    
 
  // write-in text of current dayjs value to html
-  });
-  
+
